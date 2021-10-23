f81bf29356482a5dfa548e2fce90a43610a87801
BrunoAgst <bruno_agst@hotmail.com>
2021-10-22 21:58:06 -0300

    remove comments

--- adapter-ms/src/controller/index.js
+++ adapter-ms/src/controller/index.js
@@ -2,8 +2,8 @@ const { postRequest } = require('../services/request')
 module.exports = {
     create: async (req, res) => {
         const payload = req.body
-
-        //await postRequest(payload)
+        
+        await postRequest(payload)
 
         res.status(200).json({
             message: 'transaction success',
