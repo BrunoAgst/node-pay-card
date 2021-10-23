70da20d68006a58dec8031db208d3188798958f5
BrunoAgst <bruno_agst@hotmail.com>
2021-10-22 22:51:09 -0300

    change request.js

--- adapter-ms/src/services/request.js
+++ adapter-ms/src/services/request.js
@@ -3,7 +3,6 @@ const { post } = require('axios')
 module.exports = {
     postRequest: async (payload) => {
         const url = process.env.HOST
-        const { data } = await post(url, payload)
-        return JSON.stringify(data)
+        return await post(url, { timeout: 10000 }, payload)
     }
 }
