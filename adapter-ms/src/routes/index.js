4b4cd5fa5d1ec2397bf09efb73f322653d645d1a
BrunoAgst <bruno_agst@hotmail.com>
2021-10-22 21:30:37 -0300

    installed joi and created middleware in create route

--- adapter-ms/src/routes/index.js
+++ adapter-ms/src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const handler = require('../controller')
+const middleware = require('../middleware')
 
-router.post('/create', handler.create)
+router.post('/create', middleware.create, handler.create)
 
 module.exports = router
