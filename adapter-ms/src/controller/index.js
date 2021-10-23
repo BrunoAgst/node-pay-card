7ab86bfa68961265c358f3714d4769f2f5d2bddb
BrunoAgst <bruno_agst@hotmail.com>
2021-10-22 22:51:47 -0300

    change controller and test unit

--- adapter-ms/src/controller/index.js
+++ adapter-ms/src/controller/index.js
@@ -1,13 +1,20 @@
+const logger = require('../config/logger')
 const { postRequest } = require('../services/request')
+
 module.exports = {
     create: async (req, res) => {
-        const payload = req.body
-        
-        await postRequest(payload)
-
-        res.status(200).json({
-            message: 'transaction success',
-            isValid: true
-        })
+        try {
+            const payload = req.body
+            
+            await postRequest(payload)
+    
+            res.status(200).json({
+                message: 'transaction success',
+                isValid: true
+            })
+        } catch (error) {
+            logger.error(error.message)
+            res.status(500).json({error: 'Service Unavailable', isValid: false})
+        }
     }
 }
