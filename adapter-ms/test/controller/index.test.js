7b8036906332350bb3229ea01a04835a9361e47b
BrunoAgst <bruno_agst@hotmail.com>
2021-10-22 22:50:47 -0300

    installed pino and configured pino-pretty

--- adapter-ms/test/controller/index.test.js
+++ adapter-ms/test/controller/index.test.js
@@ -4,8 +4,14 @@ const sinon = require('sinon')
 const controller = require('../../src/controller')
 
 describe('test create controller', async () => {
+    let stub = {}
+
+
+    before(() => {
+        stub = sinon.stub(controller, controller.create.name)
+    })
+
     it('return success', async () => {
-        const stub = sinon.stub(controller, controller.create.name)
             stub
                 .withArgs()
                 .returns({
@@ -16,6 +22,20 @@ describe('test create controller', async () => {
         expect(create).to.be.deep.equal({
             message: 'transaction success',
             isValid: true
+        })
+    })
+
+    it('return error', async () => {
+            stub
+                .withArgs()
+                .returns({
+                    error: 'Service Unavailable',
+                    isValid: false
+                })
+        const create = await controller.create()
+        expect(create).to.be.deep.equal({
+            error: 'Service Unavailable',
+            isValid: false
         })
     })
 })
