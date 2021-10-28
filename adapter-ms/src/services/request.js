b4d67367377cf9bc9e4b19a250105b7f2a48b7fd
BrunoAgst <bruno_agst@hotmail.com>
2021-10-28 17:30:33 -0300

    add dockerfile

--- adapter-ms/src/services/request.js
+++ adapter-ms/src/services/request.js
@@ -3,6 +3,7 @@ const { post } = require('axios')
 module.exports = {
     postRequest: async (payload) => {
         const url = process.env.HOST
-        return await post(url, { timeout: 10000 }, payload)
+        const response = await post(url, payload)
+        return response
     }
 }
