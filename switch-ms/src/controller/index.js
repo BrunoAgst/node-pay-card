63c1007363a3f4ec285302d5f6c75351c8ad8ef6
BrunoAgst <bruno_agst@hotmail.com>
2021-10-28 23:42:38 -0300

    configured rabbitmq

--- switch-ms/src/controller/index.js
+++ switch-ms/src/controller/index.js
@@ -1,10 +1,12 @@
 const logger = require('../config/logger')
+const sendQueue = require('../services/sendQueue')
 
 module.exports = {
     create: async (req, res) => {
         try {
             const payload = req.body
-                        
+            await sendQueue(payload)
+            
             res.status(200).json({
                 message: 'transaction success',
                 isValid: true
