ce298b2e4f462f95144a261823dd2c073f2bb94c
BrunoAgst <bruno_agst@hotmail.com>
2021-10-29 23:04:16 -0300

    update create controller and pollingRedis

--- idempotency-ms/src/services/poolingRedis.js
+++ idempotency-ms/src/services/poolingRedis.js
@@ -1,14 +1,16 @@
 const { createClient } = require('redis')
 const logger = require('../config/logger')
 const client = createClient()
-module.exports = async (key) => {
-    client.on('error', (err) => logger.error(err))
-    return await client.get(key, (err, res) => {
-        if (err) {
-            logger.error(err)
-            throw err
-        }
-        
-        return res
+module.exports = (key) => {
+    return new Promise((resolve, reject) => {
+        client.on('error', (err) => logger.error(err))
+        client.get(key, function(err, res) {
+            if (err) {
+                logger.error(err)
+                reject(err)
+                return
+            }
+            resolve(res)
+        })
     })
 }
