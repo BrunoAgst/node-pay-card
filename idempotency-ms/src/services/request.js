c1509b5c32c35fa751a9aee279b93dd4f855e7b3
BrunoAgst <bruno_agst@hotmail.com>
2021-11-02 22:42:00 -0300

    update postRequest

--- idempotency-ms/src/services/request.js
+++ idempotency-ms/src/services/request.js
@@ -3,7 +3,6 @@ const { post } = require('axios')
 module.exports = {
     postRequest: async (payload) => {
         const url = process.env.HOST
-        const response = await post(url, payload)
-        return response
+        return await post(url, payload)
     }
 }
